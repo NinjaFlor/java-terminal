d83688db66f48a4fe10d7b4f1d45ff2338712d03
NinjaFlor <[200~Repository>
2024-11-03 02:37:10 -0300

    Segunda tentativa de commit
    	new file:   .vscode/launch.json
    	new file:   bin/AboutMe.class
    	new file:   bin/App.class
    	new file:   bin/Calculadora.class
    	new file:   bin/Comentarios.class
    	new file:   bin/MinhaClasse.class
    	new file:   src/AboutMe.java
    	new file:   src/Calculadora.java
    	new file:   src/Comentarios.java
    	new file:   src/MinhaClasse.java

---
+++ src/Calculadora.java
@@ -0,0 +1,26 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação entre números
+ * <p>
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @autor Gleyson Sampaio
+ * @version 1.0
+ * @since 01/01/2022
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numeroUm   este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois
+     */
+
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+
+}
